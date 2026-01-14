00e79827fc442c6e8d9eef23f4c07854a3402903
Paulo-Campos-18 <pamcampos05@gmail.com>
2026-01-14 09:54:34 -0400

    Corrigindo conflito de enums
    Mesmo com mesmas chaves o enum do prisma e do domain entram em conflito, portanto uma conversão foi feita de forma a não usar o enum do prisma mantendo o código não dependente do mesmo.

--- src/infra/database/prisma/PrismaRentalRepository.ts
+++ src/infra/database/prisma/PrismaRentalRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, RentalStatus as PrismaRentalStatus } from "@prisma/client";
+import { PrismaClient, RentalStatus as PrismaRentalStatus, Rentals } from "@prisma/client";
 import { injectable } from "inversify";
 import { IRentalRepository } from "../../../domain/repositories/IRentalRepository";
 import { Rental, RentalStatus } from "../../../domain/entities/Rental";
@@ -8,20 +8,21 @@ const prisma = new PrismaClient();
 @injectable()
 export class PrismaRentalRepository implements IRentalRepository {
 
-    
-    async create(user_id: number, car_id: number, endDate: Date, status: RentalStatus): Promise<Rental> {
 
-        const rental = await prisma.rentals.create({ data: { user_id: user_id, car_id: car_id, endDate: endDate, status: PrismaRentalStatus[status] } })
-        return rental
-        
+    async create(user_id: number, car_id: number, endDate: Date, status: RentalStatus): Promise<Rental> {
+        const prismaStatus = toPrismaStatus(status)
+        const rental = await prisma.rentals.create({ data: { user_id: user_id, car_id: car_id, endDate: endDate, status: prismaStatus } })
+        return toDomainRental(rental)
     }
     async findOnGoingRentalByUserId(userId: number): Promise<Rental | null> {
-        const rental = await prisma.rentals.findFirst({ where: { user_id: userId } })
-        return rental
+        const rental = await prisma.rentals.findFirst({ where: { user_id: userId,status:PrismaRentalStatus.Andamento} })
+        if (rental === null) return null
+        return toDomainRental(rental)
     }
     async findOnGoingRentalByCarId(carId: number): Promise<Rental | null> {
-        const rental = await prisma.rentals.findFirst({ where: { car_id: carId } })
-        return rental
+        const rental = await prisma.rentals.findFirst({ where: { car_id: carId,status:PrismaRentalStatus.Andamento } })
+        if (rental === null) return null
+        return toDomainRental(rental)
     }
     remove(): void {
         throw new Error("Method not implemented.");
@@ -33,4 +34,22 @@ export class PrismaRentalRepository implements IRentalRepository {
         throw new Error("Method not implemented.");
     }
 
+}
+
+//Funções auxiliares por conta dos enums diferentes em domain e schema prisma
+//Os enum de domain e schema prisma devem ter mesmos nomes
+function toPrismaStatus(domainStatus: RentalStatus): PrismaRentalStatus {
+    return PrismaRentalStatus[domainStatus]
+}
+function toDomainStatus(prismaStatus: PrismaRentalStatus): RentalStatus {
+    return RentalStatus[prismaStatus]
+}
+function toDomainRental(prismaRental: Rentals): Rental {
+    return new Rental(
+        prismaRental.user_id,
+        prismaRental.car_id,
+        prismaRental.endDate,
+        toDomainStatus(prismaRental.status),
+        prismaRental.startDate
+    )
 }
